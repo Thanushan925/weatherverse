dadb803e3c8dfedae25fe2c57ce5eac5a50cf59f
Thanushan925 <80972937+Thanushan925@users.noreply.github.com>
2022-07-09 15:40:36 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -4,28 +4,18 @@ import axios from 'axios'
 function App() {
 
   const [data, setData] = useState({})
-  const [postalcode, setPostalCode] = useState('')
-  const [latitude, setLatitude] = useState(0)
-  const [longitude, setLongitude] = useState(0)
+  const [location, setLocation] = useState('')
 
-  const geocodingURL = `http://api.openweathermap.org/geo/1.0/zip?zip=${postalcode},CA&limit=1&appid=7fe57d3d219cd9512845e116e4c353c5`
-  const weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=7fe57d3d219cd9512845e116e4c353c5`
-  
-  const searchPostalCode = (event) => {
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=45.3848&lon=-70.8626&units=metric&appid=7fe57d3d219cd9512845e116e4c353c5`
+
+  const searchLocation = (event) => {
     if (event.key === 'Enter') {
-      axios.get(geocodingURL).then((response) => {
+      axios.get(url).then((response) => {
         setData(response.data)
         console.log(response.data)
       })
 
-      axios.get(weatherURL).then((response) => {
-        setLatitude(data.lat)
-        setLongitude(data.lon)
-        setData(response.data)
-        console.log(response.data)
-      })
-
-      setPostalCode("")
+      setLocation("")
     }
   }
 
@@ -33,11 +23,10 @@ function App() {
     <div className="app">
       <div className="search">
         <input
-          value={postalcode}
-          onChange={event => setPostalCode(event.target.value) && setLatitude(data.lat) && setLongitude(data.lon)}
-          onKeyPress={searchPostalCode}
-          searchWeather
-          placeholder='Enter Postal Code'
+          value={location}
+          onChange={event => setLocation(event.target.value)}
+          onKeyPress={searchLocation}
+          placeholder='Enter Location'
           type="text"/>
       </div>
 
@@ -52,7 +41,7 @@ function App() {
           </div>
 
           <div className="description">
-            {data.weather ? <h1>{data.weather.main}</h1> : null}
+            {data.weather ? <p>{data.weather[0].main}</p> : null}
           </div>
         </div>
 
@@ -60,17 +49,17 @@ function App() {
           <div className="bottom">
             <div className="feels">
               <p>Feels Like</p>
-              {data.main ? <h1 className='bold'>{data.main.feels_like}°C</h1> : null}
+              {data.main ? <h2 className='bold'>{data.main.feels_like}°C</h2> : null}
             </div>
 
             <div className="humidity">
               <p>Humidity</p>
-              {data.main ? <h1 className='bold'>{data.main.humidity}%</h1> : null}
+              {data.main ? <h2 className='bold'>{data.main.humidity}%</h2> : null}
             </div>
 
             <div className="wind">
               <p>Wind Speed</p>
-              {data.wind ? <h1 className='bold'>{data.wind.speed} KMPH</h1> : null}
+              {data.wind ? <h2 className='bold'>{data.wind.speed} KMPH</h2> : null}
             </div>
           </div>
         }
